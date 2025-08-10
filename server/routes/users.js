89230a92ca95f29a826ba7c6c654f89d537a5f24
Tabor Holly <89608421+taborgreat@users.noreply.github.com>
2025-08-09 22:05:37 -0700

    check login endpoint

--- server/routes/users.js
+++ server/routes/users.js
@@ -2,8 +2,14 @@ const express = require("express");
 const router = express.Router();
 
 const { register, login } = require("../controllers/users");
+const authenticate = require("../middleware/authenticate");
 
 router.post("/register", register);
 router.post("/login", login);
+
+//check if token is accurate for log in check when entering site
+router.post("/verify-token", authenticate, (req, res) => {
+  res.json({ userId: req.userId, username: req.username });
+});
 
 module.exports = router;
